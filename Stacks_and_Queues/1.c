33c863b6f731d0cbfea31bd5fcce1ce3df3607e9
Yaswanth Raj <yaswanth1030@gmail.com>
2023-11-18 23:12:57 +0530

    second commit

---
+++ Stacks_and_Queues/1.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define STACK_SIZE 3
+
+typedef struct {
+    unsigned int data[STACK_SIZE]; 
+    int top;      
+    int bit;
+} Stack;
+
+void initStack(Stack* stack)
+{
+    stack->bit = 0;
+    stack->top = 0;
+    stack->data[stack->top] = 0;
+}
+
+void push(Stack* stack, unsigned int value) 
+{
+    if (stack->top > 2)
+    {
+        printf("Stack is full\n");
+        return;
+    }    
+    if (stack->bit == 4)
+    {
+        stack->top++;
+        stack->bit = 0;
+        stack->data[stack->top] = 0;
+    }  
+    value <<= (stack->bit * 8);
+    stack->data[stack->top] |= value;
+    stack->bit++; 
+}
+
+void pop(Stack* stack)
+{
+    if (stack->top < 0)
+    {
+        printf("Stack is empty\n");
+        return;
+    }
+    printf("Memory content: 0x%X\n",stack->data[stack->top--]);
+}
+
+int main() 
+{
+    Stack stack;
+
+    initStack(&stack);
+
+    push(&stack, 0x11);
+    push(&stack, 0x22);
+    push(&stack, 0x33);
+    push(&stack, 0x44);
+
+    push(&stack, 0x10);
+    push(&stack, 0x20);
+    push(&stack, 0x30);
+    push(&stack, 0x40);
+    
+    pop(&stack);
+    pop(&stack);
+    pop(&stack);
+
+    return 0;
+}
