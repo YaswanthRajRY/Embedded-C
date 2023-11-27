ca5bb831ea03e51c9e68808b66b9c7a39bc6568e
YaswanthRajRY <151392159+YaswanthRajRY@users.noreply.github.com>
2023-11-27 22:44:20 +0530

    Update 14.c

--- Strings/14.c
+++ Strings/14.c
@@ -11,16 +11,16 @@ WTD: Use a recursive method to invert the order of characters in a string.
 
 void reverse(char* str, int start, int end)
 {
-    if (start >=end)
+    if (start >=end) // condition to exit recursion function
     {
         return;
     }
 
-    char temp = str[start];
-    str[start] = str[end];
-    str[end] = temp;
+    char temp = str[start]; // copy char to temp
+    str[start] = str[end]; // swap last char to first
+    str[end] = temp; // copy char in temp to end of the string
 
-    reverse(str, ++start, --end);
+    reverse(str, ++start, --end); // increment start index and decrement end index and call reverse function for recursion
 }
 
 int main()
