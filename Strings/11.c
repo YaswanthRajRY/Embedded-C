533c6ac7139c23595cc79a87181ed29b08b176dc
YaswanthRajRY <151392159+YaswanthRajRY@users.noreply.github.com>
2023-11-27 22:38:31 +0530

    Update 11.c

--- Strings/11.c
+++ Strings/11.c
@@ -11,24 +11,24 @@ positions were involved in the rotation.
 #include <stdlib.h>
 #include <string.h>
 
-void rotateLeft(char* str, int l)
+void rotateLeft(char* str, int l) // function to rotate string left
 {
-    char temp = str[0];
+    char temp = str[0]; // copy first char in string to temp
     for (int i = 0; i < l - 1; i++)
     {
-        str[i] = str[i + 1];
+        str[i] = str[i + 1]; // move each char in string to previous index 
     }
-    str[l - 1] = temp;
+    str[l - 1] = temp; // update first char to last in the string
 }
 
-void rotateRight(char* str, int l)
+void rotateRight(char* str, int l) // function to rotate string right
 {
-    char temp = str[l - 1];
+    char temp = str[l - 1]; // copy last char in the string to temp
     for (int i = l - 1; i > 0; i--)
     {
-        str[i] = str[i - 1];
+        str[i] = str[i - 1]; // move each char in string to next index
     }
-    str[0] = temp;
+    str[0] = temp; // update last char to first in the string
 }
 
 void rotate(char* str1, char* str2)
@@ -38,28 +38,28 @@ void rotate(char* str1, char* str2)
     char tempStr[len + 1];
     strcpy(tempStr, str1);
 
-    if (strcmp(str1, str2) == 0)
+    if (strcmp(str1, str2) == 0) // compare 2 string if equal it gives 0
     {
         printf("Both strings are the same");
         return;
     }
 
-    while (strcmp(tempStr, str2) != 0)
+    while (strcmp(tempStr, str2) != 0) // loop until both strings are equal
     {
-        rotateRight(tempStr, len);
+        rotateRight(tempStr, len); // rotate string right
         right++;
     }
 
     strcpy(tempStr, str1);
-    if (strcmp(tempStr, str2) == 0)
+    if (strcmp(tempStr, str2) == 0)  
     {
-        printf("Rotation: 0\n");
+        printf("Rotation: 0\n"); 
         return;
     }
 
-    while (strcmp(tempStr, str2) != 0)
+    while (strcmp(tempStr, str2) != 0) // loop until both strings are equal
     {
-        rotateLeft(tempStr, len);
+        rotateLeft(tempStr, len); // rotate string left
         left++;
     }
 
