3b0b5de9da1b636882f5a69655ece5daa1fd2763
YaswanthRajRY <151392159+YaswanthRajRY@users.noreply.github.com>
2023-11-27 22:30:35 +0530

    Update 10.c

--- Strings/10.c
+++ Strings/10.c
@@ -13,15 +13,15 @@ void reverse(char* str)
 {
     int i,j,len=0;
 
-    while(str[len] != '\0')
+    while(str[len] != '\0') // iterate each char untill null char
     {
-        len++;
+        len++; // increment len
     }
     
-    for (i=0, j=len-1; i<j; i++, j--)
+    for (i=0, j=len-1; i<j; i++, j--) // swaping char from first index to last
     {
         char temp = str[i];
-        str[i] = str[j];
+        str[i] = str[j]; 
         str[j] = temp;
     }
     
