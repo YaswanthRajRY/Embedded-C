593ff01e315cff8ef4ad44436de4faabfb7e47d5
YaswanthRajRY <151392159+YaswanthRajRY@users.noreply.github.com>
2023-11-27 23:04:50 +0530

    Update 20.c

--- Strings/20.c
+++ Strings/20.c
@@ -10,31 +10,31 @@ are "bca", "cab", and "abc", and the smallest is "abc".
 #include <stdio.h>
 #include <string.h>
 
-char rotateRight(char* str, int len)
+char rotateRight(char* str, int len) // rotate sting right 
 {
-    char temp = str[0];
+    char temp = str[0]; // store first char in string
     int i;
     for (i=0; i<len-1; i++)
     {
-        str[i] = str[i+1];
+        str[i] = str[i+1]; // move each char to previous index
     }
-    str[len-1] = temp;
+    str[len-1] = temp; // copy first char to last of the string
 }
 
 void lexicographyRotate(char* str, int len)
 {
-    char smallestChar = 'z';
+    char smallestChar = 'z'; // initialize larger char ASICll value
     int i;
 
-    for (i=0; i<len; i++)
+    for (i=0; i<len; i++) // find smallest char 
     {
-        if (smallestChar > str[i])
+        if (smallestChar > str[i]) 
         {
             smallestChar = str[i];
         }
     }
     
-    while (smallestChar != str[0])
+    while (smallestChar != str[0]) // rotate until first char in string is equal to found shortest char present in the string
     {
         rotateRight(str, len);
     }
