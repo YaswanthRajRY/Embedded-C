602901983506f4e4fd69f5349d469d42166b3727
YaswanthRajRY <151392159+YaswanthRajRY@users.noreply.github.com>
2023-11-27 21:54:29 +0530

    Update 02.c

--- Strings/02.c
+++ Strings/02.c
@@ -10,26 +10,26 @@ representation without relying on built-in methods.
 #include<stdio.h>
 #include<stdlib.h>
 
-int charToint(char* str)
+int charToint(char* str) //function for converting a string to its integer representation
 {
     int i=0;
     int sign = 1;
-    if (str[i] == '-')
+    if (str[i] == '-') // check if first character in sting has negative sign (ex: "-1234")
     {
         i = 1;
         sign = -1;
     }
     int digit = 0;
-    while(str[i] != '\0')
+    while(str[i] != '\0') // iterating each character untill null character
     {
-        if (str[i] >= '0' && str[i] <= '9')
+        if (str[i] >= '0' && str[i] <= '9') // condition to check the character is within the range of 0-9 in ASICll code
         {
-            int tempVar = str[i] - '0';
-            digit = digit * 10 + tempVar;
+            int tempVar = str[i] - '0'; // extracting int value from char (ex: '5' - '0' (ASICll code for 5 is 53 and for 0 is 48). Then 53 - 48 = 5)
+            digit = digit * 10 + tempVar; // update the digit
         }
-        i++;
+        i++; // move to next char in string
     }
-    return digit * sign;
+    return digit * sign; // return value with sign
 }
 
 int main()
