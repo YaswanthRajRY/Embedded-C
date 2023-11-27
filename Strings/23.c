31a42023dd39e693099b4736c9481637a95f8ef5
YaswanthRajRY <151392159+YaswanthRajRY@users.noreply.github.com>
2023-11-27 23:14:49 +0530

    Update 23.c

--- Strings/23.c
+++ Strings/23.c
@@ -16,17 +16,17 @@ void highoccurenceChar(char* str, int len)
     char c;
     int i;
 
-    for (i=0; i<len; i++)
+    for (i=0; i<len; i++) // iterate each char in the string
     {
-        charFreq[str[i]]++;
+        charFreq[str[i]]++; // count frequency of the char in the string
     }
 
-    for (i=0; i<256; i++)
+    for (i=0; i<256; i++) // find max frequecy of the char in the strig
     {
         if (charFreq[i] > maxChar)
         {
-            maxChar = charFreq[i];
-            c = i;
+            maxChar = charFreq[i]; // update max value
+            c = i; // copy ASICll code of mostl repeated char
         }
     }
     printf("%c",c);
