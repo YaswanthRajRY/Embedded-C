c8165e9e144044dae431f381bf4db64c74704595
YaswanthRajRY <151392159+YaswanthRajRY@users.noreply.github.com>
2023-11-28 19:15:04 +0530

    Update 06.c

--- Stacks_and_Queues/06.c
+++ Stacks_and_Queues/06.c
@@ -10,7 +10,7 @@ compare with the original string.
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct stack
+typedef struct stack // create struct for stack
 {
     int top;
     char* data;
@@ -18,47 +18,47 @@ typedef struct stack
 
 void initStack(Stack** stack_ref, int len)
 {
-    (*stack_ref)->data = (char*)malloc(sizeof(char) * len);
+    (*stack_ref)->data = (char*)malloc(sizeof(char) * len); // allocate memory to store char
     if ((*stack_ref)->data == NULL)
     {
         printf("Failed allocation memory\n");
     }
-    (*stack_ref)->top = 0;
+    (*stack_ref)->top = 0; // initialize 0 index for empty stack
 }
 
 void push(Stack** stack_ref, char str)
 {
-    (*stack_ref)->data[(*stack_ref)->top++] = str;
+    (*stack_ref)->data[(*stack_ref)->top++] = str; // push char in the stack and increment the top index
 }
 
 char pop(Stack** stack_ref)
 {
-    if ((*stack_ref)->top == 0)
+    if ((*stack_ref)->top == 0) // if stack empty return
     {
         printf("Stack is empty\n");
         return '\0';
     }
-    char s = (*stack_ref)->data[--(*stack_ref)->top];
+    char s = (*stack_ref)->data[--(*stack_ref)->top]; // pop the char in the stack
     return s;
 }
 
 int checkPalindrome(Stack** stack_ref, char* str)
 {
     char* str_ptr = str;
-    while (*str_ptr != '\0')
+    while (*str_ptr != '\0') // iterating each char in the string
     {
-        push(stack_ref, *str_ptr);
-        str_ptr++;
+        push(stack_ref, *str_ptr); // push each char into the stack
+        str_ptr++; // move next char in the stack
     }
-    str_ptr = str;
-    while (*str_ptr != '\0')
+    str_ptr = str; // reinitialize str pointer
+    while (*str_ptr != '\0') // iterating each char in the string
     {
-        char s = pop(stack_ref);
-        if (*str_ptr != s)
+        char s = pop(stack_ref); // save poped char in s
+        if (*str_ptr != s) // compare poped char with Str pointer first char
         {
-            return 0;
+            return 0; // return if not equal
         }
-        str_ptr++;
+        str_ptr++; // move next char in the stack
     }
 
     return 1;
@@ -66,7 +66,7 @@ int checkPalindrome(Stack** stack_ref, char* str)
 
 int main()
 {
-    Stack* stack = (Stack*)malloc(sizeof(Stack));
+    Stack* stack = (Stack*)malloc(sizeof(Stack)); // allocate memory for struct
     
     char str[] = "RACECAR";
     int len = strlen(str);
